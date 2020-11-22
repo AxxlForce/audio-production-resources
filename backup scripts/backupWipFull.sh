1d1a08a08c9d1caa2014f17dc11bebf652b80992
Alexander Stöcker <as@kempermusic.com>
2020-11-22 14:33:49 +0100

    better rsync options

--- backup scripts/backupWipFull.sh
+++ backup scripts/backupWipFull.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -rtm --include='*.RPP' --include='*.rpp' --include='*.wav' --include='*/' --exclude='*' '/Users/alex/Documents/REAPER Media/WIP/' diskstation:"'/volume1/Öffentlich/Audio Production/WIP'" --progress
+rsync -rltDOz --progress --include='*.RPP' --include='*.rpp' --include='*.wav' --include='*/' --exclude='*' '/Users/alex/Documents/REAPER Media/WIP/' diskstation:"'/volume1/Öffentlich/Audio Production/WIP'" --progress
--- backup scripts/backupWipProjectFiles.sh
+++ backup scripts/backupWipProjectFiles.sh
@@ -1,3 +1,3 @@
 #!/bin/sh
 
-rsync -rtm --include='*.RPP' --include='*.rpp' --include='*/' --exclude='*' '/Users/alex/Documents/REAPER Media/WIP' diskstation:"'/volume1/Öffentlich/Audio Production/WIP/project files'" --progress 
+rsync -rltDOz --progress --include='*.RPP' --include='*.rpp' --include='*/' --exclude='*' '/Users/alex/Documents/REAPER Media/WIP' diskstation:"'/volume1/Öffentlich/Audio Production/WIP/project files'" --progress 
